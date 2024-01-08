20c26e716c23c4fd1bdc8d7fcd66adf35043ad09
warden7383 <NgAndrew123@outlook.com>
2024-01-08 09:31:10 -0800

    feat: added teelscope keymaps

--- lua/utilities/pluginMappings.lua
+++ lua/utilities/pluginMappings.lua
@@ -4,11 +4,11 @@ vim.keymap.set('n', '<leader>d', ':NvimTreeFindFileToggle<CR>', {silent = true})
 
 --------Telescope-------
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Find Files"})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "Live Grep"})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = "Find Buffers"})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = "Find Help"})
+vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {desc = "Find Old Files"})
 
 ------Trouble -----
 -- vim.keymap.set('n', 'DD', ':TroubleToggle document_diagnostics<CR>', {silent = true}) -- enable when trouble.nvim exists

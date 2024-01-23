11bf2108099e254ffaab1fd574e7c5364bac5e8a
__warden <NgAndrew123@outlook.com>
2024-01-23 02:38:23 -0800

    bored

--- lua/plugin/gitsigns.lua
+++ lua/plugin/gitsigns.lua
@@ -35,12 +35,14 @@ require('gitsigns').setup {
   max_file_length = 40000, -- Disable if file is longer than this (in lines)
   preview_config = {
     -- Options passed to nvim_open_win
-    border = 'single',
+    border = 'rounded',
     style = 'minimal',
     relative = 'cursor',
     row = 0,
     col = 1
   },
+  trouble = true,
+  word_diff = false,
   yadm = {
     enable = false
   },

8cd70ddc30e1985293ba756cbd44eedfc3d518f0
__warden <NgAndrew123@outlook.com>
2024-03-24 23:58:22 -0700

    fix: covered case when there was no alternate buffer named #

--- lua/plugin/auto-session.lua
+++ lua/plugin/auto-session.lua
@@ -18,6 +18,18 @@
 --   },
 -- })
 
+-- Deletes the alternate buffer of '#' to prevent duplicate buffers bleeding when a session is restored
+-- NOTE: TEMP solution to bleeding buffers
+local function delAltBuf()
+  local bufNum = vim.fn.bufnr("#")
+  if bufNum == -1 then
+    -- print("No bleeding buffers")
+  else
+    -- print("Bleeding buffer found, deleting...")
+    vim.cmd("bdelete #")
+  end
+end
+
 require("auto-session").setup({
   log_level = "info",
   auto_session_enable_last_session = false,
@@ -34,7 +46,8 @@ require("auto-session").setup({
     "NvimTree",
     "checkhealth",
   },
-  post_restore_cmds = {"bdelete #"}, -- NOTE: buffer restore seems to be bleeding into the "# register(???) this is a temp. fix to deleting the buffer at "# after restoring sessions
+  -- post_restore_cmds = {"bdelete #"},
+  post_restore_cmds = {delAltBuf}, -- NOTE: buffer restore seems to be bleeding into the "# register(???) this is a temp. fix to deleting the buffer at "# after restoring sessions
 })
 
 function SessionStop()

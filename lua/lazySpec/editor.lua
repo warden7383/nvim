48819b0e4906fe0b5ce0913cdf4a8b889ac6bc1d
Andrew Ng <NgAndrew123@outlook.com>
2024-09-10 18:08:12 -0700

    feat(plugin): for markdown file note taking

--- lua/lazySpec/editor.lua
+++ lua/lazySpec/editor.lua
@@ -277,4 +277,35 @@ return {
 			require("plugin.arrow")
 		end,
 	},
+
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    lazy = true,
+    -- ft = {"md", "markdown", "txt" },
+    event = { "BufReadPre", "BufAdd" },
+    config = function ()
+      require("plugin.render-markdown")
+    end,
+    opt = {},
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter', 
+      'nvim-tree/nvim-web-devicons'
+    }, -- if you prefer nvim-web-devicons
+  },
+
+-- {
+--     "OXY2DEV/markview.nvim",
+--     lazy = false,      -- Recommended
+--     -- ft = "markdown" -- If you decide to lazy-load anyway
+--
+--     dependencies = {
+--         -- You will not need this if you installed the
+--         -- parsers manually
+--         -- Or if the parsers are in your $RUNTIMEPATH
+--         "nvim-treesitter/nvim-treesitter",
+--
+--         "nvim-tree/nvim-web-devicons"
+--     }
+-- }
+
 }

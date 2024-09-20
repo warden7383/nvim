7b062ffb78266e95002e16cea92c69c9907e5f62
__warden <NgAndrew123@outlook.com>
2024-09-19 21:42:11 -0700

    feat(lualine): finished styling lualine

--- lua/utilities/catppuccinLualine.lua
+++ lua/utilities/catppuccinLualine.lua
@@ -33,36 +33,75 @@ local colors = {
 	text1 = "#a9b1d6",
 }
 
+-- return {
+-- 	normal = {
+-- 		a = { bg = colors.blue, fg = colors.mantle, gui = "bold" },
+-- 		b = { bg = colors.surface0, fg = colors.blue },
+-- 		c = { bg = colors.black, fg = colors.text },
+-- 	},
+--
+-- 	insert = {
+-- 		a = { bg = colors.green, fg = colors.base, gui = "bold" },
+-- 		b = { bg = colors.surface0, fg = colors.green },
+-- 	},
+--
+-- 	terminal = {
+-- 		a = { bg = colors.green, fg = colors.base, gui = "bold" },
+-- 		b = { bg = colors.surface0, fg = colors.green },
+-- 	},
+--
+-- 	command = {
+-- 		a = { bg = colors.peach, fg = colors.base, gui = "bold" },
+-- 		b = { bg = colors.surface0, fg = colors.peach },
+-- 	},
+--
+-- 	visual = {
+-- 		a = { bg = colors.mauve, fg = colors.base, gui = "bold" },
+-- 		b = { bg = colors.surface0, fg = colors.mauve },
+-- 	},
+--
+-- 	replace = {
+-- 		a = { bg = colors.red, fg = colors.base, gui = "bold" },
+-- 		b = { bg = colors.surface0, fg = colors.red },
+-- 	},
+--
+-- 	inactive = {
+-- 		a = { bg = colors.transparent_bg, fg = colors.blue },
+-- 		b = { bg = colors.transparent_bg, fg = colors.surface1, gui = "bold" },
+-- 		c = { bg = colors.transparent_bg, fg = colors.overlay0 },
+-- 	},
+-- }
+
 return {
 	normal = {
-		a = { bg = colors.blue, fg = colors.mantle, gui = "bold" },
-		b = { bg = colors.surface0, fg = colors.blue },
+		a = { bg = colors.black, fg = colors.blue, gui = "bold" },
+		b = { bg = colors.black, fg = colors.blue },
 		c = { bg = colors.black, fg = colors.text },
 	},
 
 	insert = {
-		a = { bg = colors.green, fg = colors.base, gui = "bold" },
-		b = { bg = colors.surface0, fg = colors.green },
+		a = { bg = colors.black, fg = colors.green, gui = "bold" },
+		b = { bg = colors.black, fg = colors.green },
 	},
 
 	terminal = {
-		a = { bg = colors.green, fg = colors.base, gui = "bold" },
-		b = { bg = colors.surface0, fg = colors.green },
+		a = { bg = colors.black, fg = colors.green, gui = "bold" },
+		b = { bg = colors.black, fg = colors.green },
 	},
 
 	command = {
-		a = { bg = colors.peach, fg = colors.base, gui = "bold" },
-		b = { bg = colors.surface0, fg = colors.peach },
+		a = { bg = colors.black, fg = colors.peach, gui = "bold" },
+		b = { bg = colors.black, fg = colors.peach },
 	},
 
 	visual = {
-		a = { bg = colors.mauve, fg = colors.base, gui = "bold" },
-		b = { bg = colors.surface0, fg = colors.mauve },
+		a = { bg = colors.black, fg = colors.mauve, gui = "bold" },
+		b = { bg = colors.black, fg = colors.mauve },
 	},
 
 	replace = {
-		a = { bg = colors.red, fg = colors.base, gui = "bold" },
-		b = { bg = colors.surface0, fg = colors.red },
+		a = { bg = colors.black, fg = colors.red, gui = "bold" },
+		b = { bg = colors.black, fg = colors.red },
 	},
 
 	inactive = {
@@ -71,49 +110,3 @@ return {
 		c = { bg = colors.transparent_bg, fg = colors.overlay0 },
 	},
 }
--- return function(flavour)
--- 	local C = require("catppuccin.palettes").get_palette(flavour)
--- 	local O = require("catppuccin").options
--- 	local catppuccin = {}
---
--- 	local transparent_bg = O.transparent_background and "NONE" or C.mantle
---
--- 	catppuccin.normal = {
--- 		a = { bg = C.blue, fg = C.mantle, gui = "bold" },
--- 		b = { bg = C.surface0, fg = C.blue },
--- 		c = { bg = transparent_bg, fg = C.text },
--- 	}
---
--- 	catppuccin.insert = {
--- 		a = { bg = C.green, fg = C.base, gui = "bold" },
--- 		b = { bg = C.surface0, fg = C.green },
--- 	}
---
--- 	catppuccin.terminal = {
--- 		a = { bg = C.green, fg = C.base, gui = "bold" },
--- 		b = { bg = C.surface0, fg = C.green },
--- 	}
---
--- 	catppuccin.command = {
--- 		a = { bg = C.peach, fg = C.base, gui = "bold" },
--- 		b = { bg = C.surface0, fg = C.peach },
--- 	}
---
--- 	catppuccin.visual = {
--- 		a = { bg = C.mauve, fg = C.base, gui = "bold" },
--- 		b = { bg = C.surface0, fg = C.mauve },
--- 	}
---
--- 	catppuccin.replace = {
--- 		a = { bg = C.red, fg = C.base, gui = "bold" },
--- 		b = { bg = C.surface0, fg = C.red },
--- 	}
---
--- 	catppuccin.inactive = {
--- 		a = { bg = transparent_bg, fg = C.blue },
--- 		b = { bg = transparent_bg, fg = C.surface1, gui = "bold" },
--- 		c = { bg = transparent_bg, fg = C.overlay0 },
--- 	}
---
--- 	return catppuccin
--- end

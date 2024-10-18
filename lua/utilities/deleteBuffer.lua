bfe4b33df2b7c99515bc7f0c8f28beb1fb72f793
Andrew Ng <NgAndrew123@outlook.com>
2024-10-18 14:30:44 -0700

    progress for deletebuf script

--- lua/utilities/deleteBuffer.lua
+++ lua/utilities/deleteBuffer.lua
@@ -1,22 +1,49 @@
 local buffers = 0
 local window = 0
 
+function dump(o)
+	if type(o) == "table" then
+		local s = "{ "
+		for k, v in pairs(o) do
+			if type(k) ~= "number" then
+				k = '"' .. k .. '"'
+			end
+			s = s .. "[" .. k .. "] = " .. dump(v) .. ","
+		end
+		return s .. "} "
+	else
+		return tostring(o)
+	end
+end
+
 vim.api.nvim_create_user_command("BDelete", function()
 	return vim.notify("hi")
 end, { desc = "Delete the bufer without losing window layout" })
 
 vim.api.nvim_create_user_command("Test", function()
 	local buf = vim.api.nvim_win_get_buf(0)
+	local bufList = vim.api.nvim_list_bufs()
 	local wins = vim.api.nvim_list_wins()
 	local keys = vim.tbl_keys(wins)
-	local values = vim.tbl_keys(wins)
-	-- local i = 1
+	local values = vim.tbl_values(wins)
+	local filteredBufs = {}
 
-	-- for i=10,1,-1 do print(i) end
+	-- print("buflist:" .. #bufList)
+	-- for i, v in ipairs(bufList) do
+	-- 	print("KEY: " .. i .. " VALUE: " .. v)
+	-- end
 
-	for i = 1, #keys, 1 do
-		print(keys[i])
+	for i, v in ipairs(bufList) do
+		print(v .. " ")
+		if vim.api.nvim_buf_is_loaded(12) == true then
+			print("TRUE ")
+			filteredBufs[i] = v
+		end
 	end
 
-	return print(#values)
+	for i, v in ipairs(filteredBufs) do
+		print("FILTERED: " .. i .. " VALUE: " .. v)
+	end
+
+	return print("table: " .. dump(bufList))
 end, { desc = "test" })

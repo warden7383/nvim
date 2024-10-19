cad449cb08311268ce2773ca8e9e70fad3a2b80c
__warden <NgAndrew123@outlook.com>
2024-10-19 00:51:43 -0700

    fix(scrollview): attempt fix of scrollbar that stays on the window of attached buffer when window size is changed?

--- lua/plugin/scrollview.lua
+++ lua/plugin/scrollview.lua
@@ -2,7 +2,7 @@ require("scrollview").setup({
 	character = " ",
 	base = "right",
 	column = 1,
-	current_only = true,
+	current_only = false,
 	floating_windows = true, -- experimental
 	hover = true,
 	mode = "auto",
